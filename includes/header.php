e3e200cccb7bce415c6ce1da3658ef22bad56d05
jackhr <jc2o@mac.com>
2024-04-12 11:38:33 -0400

    Flesh out baseplate for site

---
+++ includes/header.php
@@ -0,0 +1,32 @@
+<?php
+
+include 'connection.php';
+
+$description = isset($description) ? $description : "Welcome to Shaq's Car Rental. We offer a wide selection of vehicles for rent. Book your car today!";
+
+$base_title = "Shaq's Car Rental";
+
+$title = isset($title_suffix) ? $base_title .= " | " . $title_suffix : $title;
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
+    <meta name="robots" content="noindex">
+    <meta name="keywords" content="car rental, affordable car rentals, car hire, vehicle rental, rent a car, car rental near me, airport car rental, luxury car hire, cheap car rental, car booking, online car rental, city car rental, car rental services, weekend car rental, business car hire, caribbean rentals, antigua, antigua and barbuda, antigua rentals">
+    <meta name="description" content="<?php echo $description ?>">
+    <meta property="og:title" content="<?php echo $base_title ?>">
+    <meta property="og:description" content="<?php echo $description; ?>">
+    <title><?php echo $title ?></title>
+    <link type="text/css" rel="stylesheet" href="/styles/main.css">
+    <?php if (isset($page) && file_exists("/styles/{$page}.css")) { ?>
+        <link type="text/css" rel="stylesheet" href="/styles/<?php echo $page ?>.css">
+    <?php } ?>
+    <script src="/js/jquery-3.7.1.min.js"></script>
+</head>
+
+<body id="<?php echo $page ?>-page">
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+$title_suffix = "Home";
+$page = "index";
+$description = "Explore the charm of Antigua's southern hideaways with Titi Vacation Homes. Immerse yourself in a personal Caribbean experience through our collection of five cozy villas nestled in the laid-back ambiance of Falmouth, Picadilly, and English Harbour. Your gateway to relaxation and tranquility awaits.";
+include_once 'includes/header.php';
+
+?>
+
+<div></div>
+
+<?php include_once 'includes/footer.php' ?>

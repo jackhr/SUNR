395f76338a4b26d0dbf937cb9b4335526acbb531
jackhr <jc2o@mac.com>
2024-04-12 16:17:43 -0400

    Update footer and main CSS styles

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,22 @@
-<footer></footer>
+<footer>
+
+    <div class="inner">
+        <div>
+            <h6>Navigation</h6>
+
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Book Now</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+
+        <div id="copyright">© 2022 Keys Car Rental Antigua   |   US$ to EC$ Exchange at 2.7169   |   For discounted rate, enter 2 or more days.</div>
+    </div>
+
+</footer>
 
 </body>
 

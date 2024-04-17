c53fb4c1448b0cda883c08e546f61d6136b7f989
jackhr <jc2o@mac.com>
2024-04-16 23:55:32 -0400

    Add testimonials section

--- index.php
+++ index.php
@@ -5,6 +5,12 @@ $page = "index";
 $description = "Explore the charm of Antigua's southern hideaways with Titi Vacation Homes. Immerse yourself in a personal Caribbean experience through our collection of five cozy villas nestled in the laid-back ambiance of Falmouth, Picadilly, and English Harbour. Your gateway to relaxation and tranquility awaits.";
 include_once 'includes/header.php';
 
+$vehicles_arr = [];
+
+$query = "SELECT * FROM vehicles";
+$result = mysqli_query($con, $query);
+while ($row = mysqli_fetch_assoc($result)) $vehicles_arr[] = $row;
+
 ?>
 
 <section id="intro-section">
@@ -147,20 +153,8 @@ include_once 'includes/header.php';
 
 </section>
 
-<?php
-
-$vehicles_arr = [];
-
-$query = "SELECT * FROM vehicles";
-$result = mysqli_query($con, $query);
-while ($row = mysqli_fetch_assoc($result)) $vehicles_arr[] = $row;
-
-?>
-
 <section id="landing-cars-section">
-
     <div class="inner">
-
         <div id="cars">
             <?php foreach ($vehicles_arr as $vehicle) { ?>
                 <div class="car-container">
@@ -213,6 +207,54 @@ while ($row = mysqli_fetch_assoc($result)) $vehicles_arr[] = $row;
             <?php } ?>
         </div>
         <div>BOOK NOW</div>
+    </div>
+</section>
+
+<section id="testimonial-section">
+
+    <div class="inner">
+
+        <div id="testimonials">
+
+            <div class="testimonial">
+                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="quote-left" class="svg-inline--fa fa-quote-left fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path d="M464 256h-80v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8c-88.4 0-160 71.6-160 160v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48zm-288 0H96v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8C71.6 32 0 103.6 0 192v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48z" />
+                </svg>
+                <div>
+                    <p>Amazing rentals this is my 3rd time renting they are always on time picking me up from the airport. Clean cars and amazing service any requests they are quick to answer!! Highly recommend!!!</p>
+                    <span>Dee Smith</span>
+                </div>
+                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="quote-right" class="svg-inline--fa fa-quote-right fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path d="M464 32H336c-26.5 0-48 21.5-48 48v128c0 26.5 21.5 48 48 48h80v64c0 35.3-28.7 64-64 64h-8c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24h8c88.4 0 160-71.6 160-160V80c0-26.5-21.5-48-48-48zm-288 0H48C21.5 32 0 53.5 0 80v128c0 26.5 21.5 48 48 48h80v64c0 35.3-28.7 64-64 64h-8c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24h8c88.4 0 160-71.6 160-160V80c0-26.5-21.5-48-48-48z" />
+                </svg>
+            </div>
+            <div class="testimonial">
+                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="quote-left" class="svg-inline--fa fa-quote-left fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path d="M464 256h-80v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8c-88.4 0-160 71.6-160 160v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48zm-288 0H96v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8C71.6 32 0 103.6 0 192v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48z" />
+                </svg>
+                <div>
+                    <p>They were very prompt and friendly. The car was exactly as advertised and no hidden costs. I would definitely use this company again.</p>
+                    <span>Derek Clive Matthews</span>
+                </div>
+                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="quote-right" class="svg-inline--fa fa-quote-right fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path d="M464 32H336c-26.5 0-48 21.5-48 48v128c0 26.5 21.5 48 48 48h80v64c0 35.3-28.7 64-64 64h-8c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24h8c88.4 0 160-71.6 160-160V80c0-26.5-21.5-48-48-48zm-288 0H48C21.5 32 0 53.5 0 80v128c0 26.5 21.5 48 48 48h80v64c0 35.3-28.7 64-64 64h-8c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24h8c88.4 0 160-71.6 160-160V80c0-26.5-21.5-48-48-48z" />
+                </svg>
+            </div>
+            <div class="testimonial">
+                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="quote-left" class="svg-inline--fa fa-quote-left fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path d="M464 256h-80v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8c-88.4 0-160 71.6-160 160v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48zm-288 0H96v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8C71.6 32 0 103.6 0 192v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48z" />
+                </svg>
+                <div>
+                    <p>Recently rented from them. Very friendly and accommodating. The car was nice and clean like new. I would definitely recommend and will rent again from The Key’s! They’re awesome.</p>
+                    <span>Barbara Ann</span>
+                </div>
+                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="quote-right" class="svg-inline--fa fa-quote-right fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path d="M464 32H336c-26.5 0-48 21.5-48 48v128c0 26.5 21.5 48 48 48h80v64c0 35.3-28.7 64-64 64h-8c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24h8c88.4 0 160-71.6 160-160V80c0-26.5-21.5-48-48-48zm-288 0H48C21.5 32 0 53.5 0 80v128c0 26.5 21.5 48 48 48h80v64c0 35.3-28.7 64-64 64h-8c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24h8c88.4 0 160-71.6 160-160V80c0-26.5-21.5-48-48-48z" />
+                </svg>
+            </div>
+
+        </div>
+
     </div>
 
 </section>

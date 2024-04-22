1c6783b5ee4f6c0aeca34a8d476be669ea18c2ff
jackhr <jc2o@mac.com>
2024-04-22 17:03:00 -0400

    Update footer

--- includes/footer.php
+++ includes/footer.php
@@ -4,40 +4,35 @@
 
     <div class="inner">
         <div>
+            <div class="footer-nav">
+                <h6>Navigation</h6>
+
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Book Now</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">FAQ</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+            <div class="footer-logo">
+                <img src="/assets/images/logo.png" alt="Site Logo">
+            </div>
             <div class="footer-contact">
                 <h6>Contact</h6>
-                <span>Belle Vue Estate</span>
-                <span>St John’s</span>
-                <span>Antigua, WI</span>
-                <div>
-                    <div class="contact-link">
-                        <span>Phone:</span>
-                        <a href="tel:+1 (268) 771-1891">+1 (268) 771-1891</a>
-                    </div>
-                    <div class="contact-link">
-                        <span>Email:</span>
-                        <a href="mailto:info@shaq.com">info@shaq.com</a>
-
-                    </div>
+                <div class="contact-link">
+                    <span>Phone:</span>
+                    <a href="tel:+1 (268) 771-1891">+1 (268) 771-1891</a>
                 </div>
-                <div class="footer-logo">
-
-                </div>
-                <div class="footer-nav">
-                    <h6>Navigation</h6>
-
-                    <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Book Now</a></li>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">FAQ</a></li>
-                        <li><a href="#">Contact</a></li>
-                    </ul>
+                <div class="contact-link">
+                    <span>Email:</span>
+                    <a href="mailto:info@shaq.com">info@shaq.com</a>
                 </div>
             </div>
-
-            <div id="copyright">© 2022 Shaquan's Car Rental Antigua   |   US$ to EC$ Exchange at 2.7169   |   For discounted rate, enter 2 or more days.</div>
         </div>
+
+        <div id="copyright">© <?php echo date('Y'); ?> Shaquan's Car Rental Antigua   |   US$ to EC$ Exchange at 2.7   |   For discounted rate, enter 2 or more days.</div>
+    </div>
 
 </footer>
 

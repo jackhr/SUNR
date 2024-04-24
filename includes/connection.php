13a21211a4d0321882013e59130dc54dfe6fa061
jackhr <jc2o@mac.com>
2024-04-24 14:51:26 -0400

    Fix connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -2,11 +2,9 @@
 
 include_once 'env.php';
 
-if ($prod) {
-    $con = mysqli_connect($hostname, $username, $password, $database);
+$con = mysqli_connect($hostname, $username, $password, $database);
 
-    if (mysqli_connect_errno()) {
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
-        exit();
-    }
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
 }
